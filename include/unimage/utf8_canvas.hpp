4ad8409043826ff041dfaefef28002423bd7aead
superbr4in <15275491+superbr4in@users.noreply.github.com>
2018-12-07 07:06:53 +0100

    Implemented UTF-8 functionality.

---
+++ include/unimage/utf8_canvas.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <map>
+#include <memory>
+
+#include <unimage/utf8_shape.hpp>
+
+namespace umg
+{
+    class utf8_canvas : std::map<int, std::vector<std::unique_ptr<utf8_shape const>>>
+    {
+        int width_;
+
+    public:
+
+        explicit utf8_canvas(int width);
+
+        void add_shape(int layer, std::unique_ptr<utf8_shape const> shape);
+
+        utf8_illustration illustrate() const;
+    };
+}
