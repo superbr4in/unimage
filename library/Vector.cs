2679a4dfb373dbd42fef62f989fc0cbab01970c3
superbr4in <15275491+superbr4in@users.noreply.github.com>
2019-01-07 09:24:39 +0100

    Implemented vector substraction.

--- library/Vector.cs
+++ library/Vector.cs
@@ -27,5 +27,11 @@ namespace Unimage
                 vector1.X + vector2.X,
                 vector1.Y + vector2.Y);
         }
+        public static Vector operator-(Vector vector1, Vector vector2)
+        {
+            return new Vector(
+                vector1.X - vector2.X,
+                vector1.Y - vector2.Y);
+        }
     }
 }
